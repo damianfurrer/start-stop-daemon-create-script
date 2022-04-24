081d2142a82bc8093829c8e03df33b18439ffef7
210mlt <210mlt@gmail.com>
2022-04-24 13:26:55 +0200

    moved vars to sourced file Added root warning to test2.sh

--- test2.sh
+++ test2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+not(){
 daemon="$(pwd)/test1.sh"
 pidfile="$(pwd)/test.pid"
 logfile="$(pwd)/test.log"
@@ -8,6 +9,9 @@ user="$(cat $useuserfile)"
 dumask="022" #u,g,o 1-7 (rwx)
 args="-p ${pidfile} -u ${user} -P -k ${dumask} ${daemon}"
 argsstart="-m -b -2 ${errfile} -1 ${logfile} ${args}"
+}
+[ $EUID -ne 0 ] || echo "Warning: Needs Root or Init privileges"
+source "$(pwd)/daemonvars.src"
 echo "${args}"
 start-stop-daemon -S $argsstart
 echo "waiting 30s to stop"
